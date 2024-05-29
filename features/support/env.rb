a7b764e612c177d1b340a138c75eae804b301e2e
Lucas Vidal <lcv55@hotmail.com>
2024-05-29 18:18:56 -0300

    Add Upright as the host

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,8 +3,11 @@ require 'capybara/cucumber'
 require "capybara/cuprite"
 
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800], browser_options: { 'no-sandbox': nil }, timeout: 10)
+  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800],
+                                     browser_options: { 'no-sandbox': nil,
+                                                        'ignore-certificate-errors': nil },
+                                     timeout: 10, headless: false)
 end
 
 Capybara.default_driver = :cuprite
-Capybara.app_host = 'https://bancoabc-dev1.numerixms.com'
+Capybara.app_host = 'https://app.linhaetica.com.br/'
