5b9c294b18a275ba76bbce136e362e98218e6343
Lucas Vidal <lcv55@hotmail.com>
2024-07-20 12:35:12 -0300

    Update pdf parser to handle category line breaks

--- features/support/pdf_parser.rb
+++ features/support/pdf_parser.rb
@@ -13,6 +13,7 @@ end
 
 def parse_transaction_details(text)
   transactions = []
+  text = remove_line_breaks_from_category(text)
 
   text.each_line do |line|
     next if line.include? 'SYMBOL'
@@ -30,4 +31,10 @@ def parse_statement_period(text)
   date_regex = /\b(January|February|March|April|May|June|July|August|September|October|November|December)\s\d{1,2}-\d{1,2},\s\d{4}\b/
   match_data = text.match(date_regex)
   match_data[0] if match_data
+end
+
+def remove_line_breaks_from_category(text)
+  text.gsub("NRA Tax\n", 'NRA Tax')
+      .gsub("Cash Dividend\n", 'Cash Dividend')
+      .gsub("Non-Qualified Div\n", 'Non-Qualified Div')
 end
